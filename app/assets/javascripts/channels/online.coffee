5fdc75ecabbaab762369667c2543595aea5627ce
zal-lex <leshik89@gmail.com>
2020-05-05 18:06:58 +0300

    Make real-time updating users online list

--- app/assets/javascripts/channels/online.coffee
+++ app/assets/javascripts/channels/online.coffee
@@ -9,3 +9,5 @@ App.online = App.cable.subscriptions.create "OnlineChannel",
 
   received: (data) ->
     # Called when there's incoming data on the websocket for this channel
+    console.log('Received online: ' + data.users.join(', '))
+    $('#online').text(data.users.join(' '))
--- app/channels/online_channel.rb
+++ app/channels/online_channel.rb
@@ -1,11 +1,20 @@
 class OnlineChannel < ApplicationCable::Channel
   def subscribed
+    stream_from "online_channel"
     current_user.update(online: true)
+    broadcast_users
     logger.info "#{current_user} Subscribed to OnlineChannel. His online status: #{current_user.online}"
   end
 
   def unsubscribed
     current_user.update(online: false)
+    broadcast_users
     logger.info "#{current_user} Unsubscribed to OnlineChannel. His online status: #{current_user.online}"
+  end
+
+  private
+
+  def broadcast_users
+    ActionCable.server.broadcast "online_channel", users: User.online.map(&:nickname)
   end
 end
