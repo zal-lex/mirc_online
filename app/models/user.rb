f083d83614a67e1287db31ee1e57251268061475
zal-lex <leshik89@gmail.com>
2020-05-05 16:39:29 +0300

    Show online users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   before_create :generate_nickname
 
+  scope :online, -> { where(online: true) }
+
   def generate_nickname
     self.nickname = Faker::Name.first_name.downcase
   end
